4722b6aaf537f129055c2c824d1d937ae6fd2815
unknown <amir.bakhtiari91@gmail.com>
2025-12-19 20:09:59 +0330

    add update role func

--- internal/adapters/repository/postgres/role_repo.go
+++ internal/adapters/repository/postgres/role_repo.go
@@ -58,19 +58,33 @@ func (r *RoleRepository) List(ctx context.Context) ([]*domain.Role, error) {
 		Find(&roleModels).Error; err != nil {
 		return nil, err
 	}
+
 	roles := make([]*domain.Role, 0, len(roleModels))
 
-	for _, m := range roles {
-		roles = append(roles, mappers.RoleModelToDomain(&m))
+	for _, m := range roleModels {
+		role := mappers.RoleModelToDomain(&m)
+		roles = append(roles, role)
 	}
 	return roles, nil
 }
 func (r *RoleRepository) Delete(ctx context.Context, id uint) error {
-
+	if err := r.db.WithContext(ctx).
+		Delete(&models.Role{}, id).Error; err != nil {
+		return err
+	}
+	return nil
 }
 
 func (r *RoleRepository) Update(ctx context.Context, role *domain.Role) error {
+	model := mappers.RoleDomainToModel(role)
 
+	if err := r.db.WithContext(ctx).
+		Model(&models.Role{}).
+		Where("id=?", role.ID).
+		Updates(model).Error; err != nil {
+		return err
+	}
+	return nil
 }
 func (r *RoleRepository) GetByID(ctx context.Context, id uint) (*domain.Role, error) {
 	var role domain.Role
